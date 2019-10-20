da03dc2170be312bb4458cbd365414f78ecf4698
malcolmt <malcolmt@kth.se>
2019-10-20 16:29:17 -0500

    search engine is complete

--- search_engine.py
+++ search_engine.py
@@ -14,8 +14,8 @@ def get_top_k_docs(query, k=10):
     Returns:
         top_k_docs: dictionary keys: titles, abstracts, urls, ids. Each element in dict[key] is a list of k elements in descending order of relevance
     """
-    tokenized_query = preprocess_query(query)
-    corpus, tokenized_corpus = read_file()
+    tokenized_query = p.preprocess_query(query)
+    corpus, tokenized_corpus = p.read_file()
 
     bm25 = BM25Plus(tokenized_corpus)
     top_k_docs = bm25.get_top_n(tokenized_query, corpus, n=k)
@@ -29,4 +29,4 @@ def store_relevance_judgements():
     pass
 
 
-corpus, tokenized_corpus = p.read_file()
+#topk = [line["title"] for line in get_top_k_docs("natural search", 3)]
