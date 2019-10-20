bcc5ef37f7394f7542ca83e7ed86ba833f539427
malcolmt <malcolmt@kth.se>
2019-10-20 14:32:56 -0500

    Migrating model code to search_engine

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 
 from rank_bm25 import BM25Okapi
+from rank_bm25 import BM25Plus
 
 test_corpus = [
     "Hello there good man",
@@ -19,7 +20,7 @@ else:
 	model = ''
 
 if model == 'bm25':
-	bm25 = BM25Okapi(tokenized_corpus)
+	bm25 = BM25Plus(tokenized_corpus)
 	doc_scores = bm25.get_scores(tokenized_query)
 	doc_scores[::-1].sort()
 	top_10_doc = bm25.get_top_n(tokenized_query, test_corpus, n=10)
